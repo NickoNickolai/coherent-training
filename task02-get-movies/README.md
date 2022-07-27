8f208bfcb1ac5b49c390b88b488c1e53fd744218
Nickolai <kolano@inbox.ru>
2022-07-27 15:47:23 +0300

    Review task02-get-movies: - exception flow fixed - line length < 120 - added sorted_movies() function - console encoding set to utf-8 - uninformative identifiers fixed

--- task02-get-movies/README.md
+++ task02-get-movies/README.md
@@ -4,22 +4,31 @@ Pure python utility to get top n movies by each genre from csv-data.
 Outputs to the stdout in csv-like format: (genre, title, year, rating).
 Source filepaths specified in config file.
 
+## Requirements
 
-## Options
+The utility requires [**`python3`**](https://www.python.org/downloads/) interpreter with [**`pip`**](https://pypi.org/project/pip/) installing tool.
 
-All options are optional.
+On the command line the interpreter can be typed as `python`, `python3`, `py` (depending on OS, version, etc.).
 
-- **--N** *<n>* — number filter, limit movies count for output
-- **--genres** *<genres list>* — genres filter, list of genres separated by '|'
-- **--year_from** *<year>* — year-from filter
-- **--year_to** *<year>* — year-to filter
-- **--regexp** *<regexp>* — regexp filter for title
-- **--help** — show this help message and exit
+To be specific this readme has decided to use the interpreter name `python` in the examples.
 
 ## Usage
 
+All options are optional. To show help message below use `--help` option.
+
 ```sh
-get-movies.py [--N <number>] | [--genres <list>] | [--year_from <year>] | [--year_to <year>] | [--regexp <regular-expression>] | [--help]
+usage: get-movies.py [--N <number>] [--genres <list>] [--year_from <year>] [--year_to <year>] [--regexp <regexp>] [--help]
+
+Pure python utility to get top n movies by each genre from csv data. Outputs to the stdout in csv-like format: (genre, title,     
+year, rating). Source filepaths specified in config file.
+
+options:
+  --N <number>        top rated movies count for each genre
+  --genres <list>     genres filter, list separated by '|'
+  --year_from <year>  year-from filter
+  --year_to <year>    year-to filter
+  --regexp <regexp>   regexp filter for title
+  --help              show this help message and exit
 ```
 
 All filters can be combined in any combination.
@@ -27,10 +36,10 @@ Output is always grouped by genre and sorted by rating DESC, year DESC, title AS
 
 ## Examples
 
-Get all movies for each genre:
+- Get all movies for each genre
 ```sh
-get-movies.py
-
+> python get-movies.py
+  
 genre,title,year,rating
 Action,Tokyo Tribe,2014,5.0
 Action,Justice League: Doom,2012,5.0
@@ -40,10 +49,10 @@ Action,Superman/Batman: Public Enemies,2009,5.0
 ...
 ```
 
-Get 3 top rated movies for both "Sci-Fi" and "War" genres:
+- Get 3 top rated movies for both "Sci-Fi" and "War" genres:
 ```sh
-get-movies.py --N 3 --genres "Sci-Fi|War"
-
+> python get-movies.py --N 3 --genres "Sci-Fi|War"
+  
 genre,title,year,rating
 Sci-Fi,SORI: Voice from the Heart,2016,5.0
 Sci-Fi,The Girl with All the Gifts,2016,5.0
@@ -53,9 +62,9 @@ War,Che: Part One,2008,5.0
 War,Che: Part Two,2008,5.0
 ```
 
-Get 3 top rated movies with title containing "the" or "The" for "Sci-Fi" genre released from 1999 to 2000 year:
+- Get 3 top rated movies with title containing "the" or "The" for "Sci-Fi" genre released from 1999 to 2000 year:
 ```sh
-get-movies.py --N 3 --regexp ".[Tt]he " --genre "Sci-Fi" --year_from 1999 --year_to 2000
+> python get-movies.py --N 3 --regexp ".[Tt]he " --genre "Sci-Fi" --year_from 1999 --year_to 2000
 
 genre,title,year,rating
 Sci-Fi,Batman Beyond: Return of the Joker,2000,3.5
@@ -63,29 +72,29 @@ Sci-Fi,Star Wars: Episode I - The Phantom Menace,1999,3.107142857142857
 Sci-Fi,Universal Soldier: The Return,1999,2.625
 ```
 
-Show help message:
-```sh
-convert.py --help
-```
-
 ## Configuration
 
-Configuration file **config.ini** should be stored next to the script. It should contain:
+Configuration file `config.ini` should be stored next to the script. It should contain:
 
-#### [Source]
-- **movies_path** — input movies.csv filepath
-- **ratings_path** — input ratings.csv filepath
-- **encoding** — input files encoding
-- **delimiter** — input files delimiter
-#### [Destination]
-- **encoding** — output data encoding
-- **delimiter** — output data delimiter
-- **write_schema** — enable output schema 
-#### [Settings]
-- **title_regexp** — regular expression to split raw title into the real title and year
-- **no_genres_regexp** — regular expression to detect movies with no genre
+**[Source]**
 
-## Requirements
+- `movies_path` — input movies.csv filepath
+- `ratings_path` — input ratings.csv filepath
+- `encoding` — input files encoding
+- `delimiter` — input files delimiter
+
+**[Destination]**
+
+- `encoding` — output data encoding
+- `delimiter` — output data delimiter
+- `write_schema` — enable output schema 
+
+**[Extraction]**
+
+- `title_regexp` — regular expression to split raw title into the real title and year
+- `no_genres_regexp` — regular expression to detect movies with no genre
+
+## Source data
 
 Source files should be downloaded from [grouplens.org](https://grouplens.org/datasets/movielens/):
 
@@ -93,11 +102,14 @@ Source files should be downloaded from [grouplens.org](https://grouplens.org/dat
 - [MovieLens Latest Dataset](https://files.grouplens.org/datasets/movielens/ml-latest.zip)
 - [MovieLens 25M Dataset](https://files.grouplens.org/datasets/movielens/ml-25m.zip)
 
-Example files included in the **data/** directory.
+Example files included in the `data/` directory.
 
 ### Download utility
 
-Usage of the additional shell utilitiy to automatically download source files:
+Additional shell utility allowes to download and extract source files automatically.
+
+Usage of the utility:
+
 ```sh
 setup.sh --ds small|medium|stable
 ```
--- task02-get-movies/get-movies.py
+++ task02-get-movies/get-movies.py
@@ -34,27 +34,24 @@ def configure():
         config['dst_delimiter'] = parser.get('Destination', 'delimiter')
         config['write_schema'] = int(parser.get('Destination', 'write_schema'))
 
-        config['title_regexp'] = parser.get('Settings', 'title_regexp')
-        config['no_genres_regexp'] = parser.get('Settings', 'no_genres_regexp')
+        config['title_regexp'] = parser.get('Extraction', 'title_regexp')
+        config['no_genres_regexp'] = parser.get('Extraction', 'no_genres_regexp')
     except Exception:
-        sys.stderr.write("Exception: corrupted config file")
-        sys.exit(-1)
+        raise Exception("corrupted config file")
 
 
 def create_parser():
     """
-    Create configured parser for CLI arguments.
+    Return configured parser for CLI arguments.
     """
-    usage = "get-movies.py [--N <number>] | [--genres <list>] | [--year_from <year>] | [--year_to <year>] | [--regexp <regular-expression>] | [--help]"
-    description = "Select top n of the most rated movies."
-    parser = argparse.ArgumentParser(usage=usage, description=description, add_help=False)
+    parser = argparse.ArgumentParser(description=__doc__, add_help=False)
 
-    parser.add_argument("--N", metavar='', help="quantity of the most rated movies")
-    parser.add_argument("--genres", metavar='', help="genres filter")
-    parser.add_argument("--year_from", metavar='', help="start year")
-    parser.add_argument("--year_to", metavar='', help="end year")
-    parser.add_argument("--regexp", metavar='', help="regexp filter")
-    parser.add_argument("--help", action='store_true', help="show this help message and exit")
+    parser.add_argument("--N", metavar="<number>", help="top rated movies count for each genre")
+    parser.add_argument("--genres", metavar="<list>", help="genres filter, list separated by '|'")
+    parser.add_argument("--year_from", metavar="<year>", help="year-from  filter")
+    parser.add_argument("--year_to", metavar="<year>", help="year-to  filter")
+    parser.add_argument("--regexp", metavar="<regexp>", help="regexp filter for title")
+    parser.add_argument("--help", action="store_true", help="show this help message and exit")
 
     return parser
 
@@ -64,8 +61,12 @@ def calc_avg_rating():
     Calculate average rating from ratings csv-file.
     Return average rating storage: { movieId: avg_rating }
     """
-    with open(config['ratings_fpath'], encoding=config['src_encoding']) as ratings_file:
-        reader = csv.DictReader(ratings_file, delimiter=config['src_delimiter'])
+    filepath = config['ratings_fpath']
+    encoding = config['src_encoding']
+    delimiter = config['src_delimiter']
+
+    with open(filepath, encoding=encoding) as ratings_file:
+        reader = csv.DictReader(ratings_file, delimiter=delimiter)
 
         # Raw ratings storage: { movieId: { total, count } }
         rating_storage = {}
@@ -76,8 +77,7 @@ def calc_avg_rating():
                 movieId = int(row['movieId'])
                 rating = float(row['rating'])
             except KeyError:
-                sys.stderr.write("KeyError: failed to extract data")
-                sys.exit(-1)
+                raise KeyError("failed to extract data")
 
             # Store current row rating
             if movieId in rating_storage:
@@ -98,29 +98,34 @@ def calc_avg_rating():
 
 def split_title(raw_title):
     """
-    Split raw title string into the real title and year.
+    Split `raw_title` string into the real title and year.
+    Return them as a tuple of (title, year).
     """
+    title_regexp = config['title_regexp']
+
     raw_title = raw_title.strip()
-    re_result = re.search(config['title_regexp'], raw_title)
-    try:
-        title = re_result.groups()[0]
-        year = int(re_result.groups()[1])
-    except:
-        raise Exception
+    re_result = re.search(title_regexp, raw_title)
+
+    re_title, re_year = re_result.groups()
+    title, year = re_title, int(re_year)
 
     return title, year
 
 
 def split_genres(raw_genres):
     """
-    Split raw genres string into the genres list.
+    Split `raw_genres` string into the genres list.
+    Return genres list.
     """
+    no_genres_regexp = config['no_genres_regexp']
+
     raw_genres = raw_genres.strip()
-    re_result = re.search(config['no_genres_regexp'], raw_genres)
-    if not re_result:
-        return raw_genres.split('|')
-    else:
-        raise Exception
+    re_result = re.search(no_genres_regexp, raw_genres)
+
+    if re_result:
+        raise Exception("invalid genre")
+
+    return raw_genres.split('|')
 
 
 def extract_movies():
@@ -130,8 +135,12 @@ def extract_movies():
     """
     rating_storage = calc_avg_rating()
 
-    with open(config['movies_fpath'], encoding=config['src_encoding']) as movies_file:
-        reader = csv.DictReader(movies_file, delimiter=config['src_delimiter'])
+    filepath = config['movies_fpath']
+    encoding = config['src_encoding']
+    delimiter = config['src_delimiter']
+
+    with open(filepath, encoding=encoding) as movies_file:
+        reader = csv.DictReader(movies_file, delimiter=delimiter)
 
         # Movies storage: [ { movieId, title, year, genre, rating } ]
         # List type chosen because of next sorting
@@ -144,7 +153,7 @@ def extract_movies():
                 title, year = split_title(row['title'])
                 genre_list = split_genres(row['genres'])
                 rating = rating_storage[movieId]
-            except:
+            except Exception:
                 # Skip bad data
                 continue
 
@@ -160,30 +169,39 @@ def extract_movies():
     return movies_storage
 
 
-def filter_movies(filters):
+def sorted_movies():
     """
-    Store movies filtered with `filters`.
+    Sort movies by genre ASC, rating DESC, year DESC, title ASC
+    Return sorted movies storage.
     """
-    try:
-        movies_storage = extract_movies()
-    except Exception as e:
-        sys.stderr.write(f"Exception: {e}")
-        sys.exit(-1)
+    movies_storage = extract_movies()
 
-    # Group by genre, sort by rating DESC, year DESC, title ASC (in reversed order)
-    # Built-in sort works much faster than implemented with pure python cycles, but requies additional memory
+    # Group by genre, sort by rating DESC, year DESC, title ASC.
+    # Sortings should be in reversed order.
+    # Built-in sort works much faster than implemented
+    # with pure python cycles, but requies additional memory
     movies_storage.sort(key=lambda item: item['title'])
     movies_storage.sort(key=lambda item: item['year'], reverse=True)
     movies_storage.sort(key=lambda item: item['rating'], reverse=True)
     movies_storage.sort(key=lambda item: item['genre'])
 
+    return movies_storage
+
+
+def filter_movies(filters):
+    """
+    Filter movies by `filters` dictionary and store them into a deque.
+    Return movies storage.
+    """
+    movies_storage = sorted_movies()
     result_storage = deque()
 
-    # When limit not specified all the movies should go to the output
+    # N is the movies count for each genre specified
+    # When it's not specified all the movies should go to the output
     if filters['N'] is None:
-        movies_limit = len(movies_storage)
+        N = len(movies_storage)
     else:
-        movies_limit = filters['N']
+        N = filters['N']
 
     # Extract all genres into a set
     genres_list = {movie['genre'] for movie in movies_storage}
@@ -191,12 +209,12 @@ def filter_movies(filters):
     # Each genre should be count separately into this counter
     genre_counter = {genre: 0 for genre in genres_list}
 
-    # Total movies limit is movies_limit * genres_size
-    # When genres filter not specified all the genres should go to the output
+    # Total movies limit is genres_size * N
+    # When genres filter is not specified all the genres should go to the output
     if filters['genres'] is None:
-        total_limit = len(genres_list) * movies_limit
+        total_limit = len(genres_list) * N
     else:
-        total_limit = len(filters['genres']) * movies_limit
+        total_limit = len(filters['genres']) * N
 
     matched_movie_counter = 0
 
@@ -217,7 +235,7 @@ def filter_movies(filters):
                 continue
 
         # Check each genre output limit
-        if genre_counter[movie['genre']] >= movies_limit:
+        if genre_counter[movie['genre']] >= N:
             continue
 
         # Filter `regexp` for title
@@ -247,10 +265,8 @@ def main():
     """
     Entry point: configure script, get CLI args and process target.
     """
-    configure()
-
-    parser = create_parser()
-    args = vars(parser.parse_args(sys.argv[1:]))
+    # Set console encoding to UTF-8
+    sys.stdout.reconfigure(encoding='utf-8')
 
     filters = {'N': None,
                'genres': None,
@@ -259,30 +275,44 @@ def main():
                'regexp': None}
 
     try:
+        configure()
+
+        parser = create_parser()
+        args = vars(parser.parse_args())
+
         if args['help']:
-            sys.stdout.write(parser.format_help())
-            return
+            print(parser.format_help(), file=sys.stdout)
+            sys.exit(0)
+
         if args['N'] is not None:
             filters['N'] = int(args['N'])
+
         if args['genres'] is not None:
             filters['genres'] = split_genres(args['genres'])
+
         if args['year_from'] is not None:
             filters['year_from'] = int(args['year_from'])
+
         if args['year_to'] is not None:
             filters['year_to'] = int(args['year_to'])
+
         if args['regexp'] is not None:
             filters['regexp'] = args['regexp']
-    except OSError as e:
-        sys.stderr.write(f"FileError: {e}\n")
-    except BaseException as e:
-        sys.stderr.write(f"Exception: {e}\n")
 
-    found_movies = filter_movies(filters)
+        found_movies = filter_movies(filters)
+
+    except Exception as e:
+        print(f"Exception: {e}", file=sys.stderr)
+        sys.exit(1)
 
     headers = ['genre', 'title', 'year', 'rating']
-    writer = csv.DictWriter(sys.stdout, headers, delimiter=config['dst_delimiter'])
+    delimiter = config['dst_delimiter']
+    writer = csv.DictWriter(sys.stdout, headers,
+                            delimiter=delimiter,
+                            lineterminator='\n')
 
-    if config['write_schema']:
+    write_schema = config['write_schema']
+    if write_schema:
         writer.writeheader()
 
     # Output the found data
